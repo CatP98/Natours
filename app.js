3065d1d7762f1113b0a1d70b5d02d07d955c2c01
catarina p <catarinampestana@ua.pt>
2024-09-23 12:56:47 +0100

    Handling with PATCH requests

--- app.js
+++ app.js
@@ -71,6 +71,22 @@ app.post('/api/v1/tours', (req, res) => {
         })
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length){ 
+        return res.status(404).json({
+            status: "fail",
+            message: "Id Not Found"
+        });
+    }
+   
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour: '<Updated Tour Here>' //placeholder
+        }
+    });
+})
+
 
 //Start the server
 const port = 3000;
