1a091e35c6b69da0925c9646ad2e7bf1dd76d144
catarina p <catarinampestana@ua.pt>
2024-09-23 15:20:53 +0100

    using 3rd party middleware -> Morgan

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
 
 const app = express(); //a function which upon calling will add a bunch of methods and proprieties to app varaible
 
+/////////////////////////////////////////////////////////////// 1. MIDDLEWARES//////////////////////////////////////////////////////////
+
 //Include the mniddleware -> what gives us access to the request data for a post, from the client
+app.use(morgan('dev'));
+
 app.use(express.json()); //'express.json()' is middleware -> a funtion that can modify the incoming request data. A step that the request goes through to be processed
 
 app.use((req, res, next) => { //like this, Express knows that we are defining a middleware funtion in here. Wuth the 3rd  arameter, express passes the next function into the middleware function and we can use it whenever we want it
@@ -21,6 +27,8 @@ app.use((req, res, next) => { //like this, Express knows that we are defining a 
 const tours =  JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+/////////////////////////////////////////////////////////////// 2. ROUTE HANDLERS ///////////////////////////////////////////////////////////////
 
 const getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -110,13 +118,14 @@ const deleteTour = (req, res) => {
     });
 };
 
-//The Route handlers
+
 //app.get('/api/v1/tours', getAllTours);
 //app.get('/api/v1/tours/:id/:x?', getTourById);
 //app.post('/api/v1/tours', createTour);
 //app.patch('/api/v1/tours/:id', updateTour);
 //app.delete('/api/v1/tours/:id', deleteTour);
 
+/////////////////////////////////////////////////////////////// 3. ROUTES ///////////////////////////////////////////////////////////////
 app.
     route('/api/v1/tours')
     .get(getAllTours)
@@ -130,7 +139,7 @@ app.
 
 
 
-//Start the server
+/////////////////////////////////////////////////////////////// 4. SERVER ///////////////////////////////////////////////////////////////
 const port = 3000;
 //to start up a server   // () => {...} the callback function that will be called as soon as the server starts listening
 app.listen( port, () => {
