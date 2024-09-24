cea41b1f8e1d4c1321072652e34364eefd28e613
catarina p <catarinampestana@ua.pt>
2024-09-24 10:36:25 +0100

    practicing param middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,10 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();  //tourRouter is a middleware and we want to use this specific middleware for the '/api/v1/tours' url -> it's like creatin a sub-app. THhe tourRouter only runs in this route '/api/v1/tours'
 
+router.param('id', (req, res, next, val) => { //Middleware router that is only specified for tour router with is param
+    console.log(`Tour id is : ${val}`);
+    next();
+} );
 
 // ROUTES
 router
