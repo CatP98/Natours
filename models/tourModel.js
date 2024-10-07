2016c960e7aaed42de0876fec07ce2abd1c57d13
catarina p <catarinampestana@ua.pt>
2024-10-07 11:46:53 +0100

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -58,7 +58,11 @@ const tourSchema = new mongoose.Schema(
 			default: Date.now(),
 			select: false
 		},
-		startDates: [Date]
+		startDates: [Date],
+		secretTour: {
+			type: Boolean,
+			default: false
+		}
 	},
 	{
 		toJSON: {
@@ -75,6 +79,38 @@ tourSchema.virtual('durationWeeks').get(function () {
 	//Using the regular function, because an arrow function does not get access to the this keyword. The this will be pointing to the current document
 	return this.duration / 7;
 });
+
+//QUERY DOCUMENT
+tourSchema.pre('find', function (next) {
+	console.log('====================');
+	console.log('Query middleware, here is the this, the query object: ');
+	console.log('====================');
+	console.log(this);
+
+	console.log('====================');
+	console.log('This line should print after the query object log.');
+	next();
+});
+
+// tourSchema.pre('find', function (next) {
+// 	this.find({ secretTourTour: { $ne: true } });
+// 	next();
+// });
+
+//the query middleware creates a query object, and from the moment of the pre hook until the post hook of a certain query, we get access to the same query object, with its set properties and methods, that we can use/access anytime we want in this process
+tourSchema.pre(/^find/, function (next) {
+	this.find({ secretTour: { $ne: true } });
+
+	this.start = Date.now(); //Setting a property with the time the object was creted and store it in the object on the 'start' attribute
+	next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+	console.log(`This query took ${Date.now() - this.start} miliseconds`); //This will log the time it took from the moment the find query was triggered (right before the query got executes), until the moment the query finnished executing and, therefore, started the post middleware
+	console.log(docs); // This will  log to the console all the documents that resulted in the query that started with 'find'
+	next();
+});
+
 // DOCUMENT MIDDLEWARE,
 // PRE-HOOKS: runs before .save() and .create() (other  like insertMant() for example will not trigger the function callback)
 tourSchema.pre('save', function (next) {
